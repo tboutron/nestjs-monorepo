d36ed6dd67052422da9bbfb5a89fcea40bfbee45
tboutron <tboutron@stot.fr>
2024-01-18 21:20:10 +0100

    ci: adding CI to valid: lint, unit & e2e testing

--- node_modules/@app/auth.api/src/modules/login/adapter.ts
+++
@@ -1,5 +0,0 @@
-import { UserEntity } from '../user/entity';
-
-export abstract class ILoginService {
-  abstract login(user: UserEntity): Promise<UserEntity>;
-}
--- node_modules/@libs/modules/cache/types.ts
+++
@@ -1,7 +0,0 @@
-export type CacheKeyArgument = string | Buffer;
-export type CacheValeuArgument = string | Buffer;
-
-export type CacheKeyValue = {
-  key: CacheKeyArgument;
-  value: CacheValeuArgument | CacheValeuArgument[];
-};
